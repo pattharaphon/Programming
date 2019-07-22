74dd52dc6e6bcf412fb7be10c2c6f0f8139f25f4
pattharaphon <52312568+pattharaphon@users.noreply.github.com>
2019-07-22 10:35:36 +0700

    Add files via upload

---
+++ Exercise8_Pattharaphon_S.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Usernameinput = input("Username :")
+Passwordinput = input("Password:")
+if Usernameinput=="Pattharaphon" and Passwordinput=="258369":
+    print("Wecome To Pattharaphon Coffee")
+    print("What do want to buy something?")
+    print("This is all Menu in Pattharaphon Coffee")
+    print("******************************")
+    print("1.ชานมไข่มุก         35 THB")
+    print("2.ชานมโกโก้         40 THB")
+    print("3.แดงมะนาวโซดา  25 THB")
+    print("4.เอสเพรสโซ่        45 THB")
+    print("******************************")
+    Cargo=int(input("Do you want to buy?"))
+    
+    if Cargo==1:
+        value1=35
+        print("Your Cargo:",Cargo," = ",value1,"THB")
+    elif Cargo==2:
+        value1=40
+        print("Your Cargo:",Cargo," = ",value1,"THB")
+    elif Cargo==3:
+        value1=25
+        print("Your Cargo:",Cargo," = ",value1,"THB")
+    elif Cargo==4:
+        value1=45
+        print("Your Cargo:",Cargo," = ",value1,"THB")
+    Qty1 =int(input("Do you want to Qty?"))
+    
+    if Cargo==1:
+        value1=35
+        print("Total:",Cargo," = ",value1*Qty1,"THB")
+    elif Cargo==2:
+        value1=40
+        print("Total:",Cargo," = ",value1*Qty1,"THB")
+    elif Cargo==3:
+        value1=25
+        print("Total:",Cargo," = ",value1*Qty1,"THB")
+    elif Cargo==4:
+        value1=45
+        print("Total:",Cargo," = ",value1*Qty1,"THB")
+    Cargo=int(input("Do you want to buy?"))
+
+    if Cargo==1:
+        value2=35
+        print("Your Cargo:",Cargo," = ",value2,"THB")
+    elif Cargo==2:
+        value2=40
+        print("Your Cargo:",Cargo," = ",value2,"THB")
+    elif Cargo==3:
+        value2=25
+        print("Your Cargo:",Cargo," = ",value2,"THB")
+    elif Cargo==4:
+        value2=45
+        print("Your Cargo:",Cargo," = ",value2,"THB")
+    Qty2 =int(input("Do you want to Qty?"))
+
+    if Cargo==1:
+        cost2=35
+        print("Total:",Cargo," = ",value2*Qty2,"THB")
+    elif Cargo==2:
+        cost2=40
+        print("Total:",Cargo," = ",value2*Qty2,"THB")
+    elif Cargo==3:
+        cost2=25
+        print("Total:",Cargo," = ",value2*Qty2,"THB")
+    elif Cargo==4:
+        cost2=45
+        print("Total:",Cargo," = ",value2*Qty2,"THB")
+    print("All Totall=",value1*Qty1+value2*Qty2,"THB")
+else:
+    print("ERROR")
+
+
+# In[ ]:
+
+
+
+
---
+++ HW2_1_Pattharaphon_S.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''
+Pattharaphon Suwanjarern 362515241020
+'''
+print("           MENU          ")
+print("Welcome to icecream shop")
+print("1.Strawberry 80 THB")
+print("2.Vanilla       50 THB")
+print("3.Chocalate  40 THB")
+print("4.Lemon      90 THB")
+print("5.Milk          10 THB")
+print("------------------")
+ch=int(input("Do you want ?"))
+
+if ch==1:
+    price=80
+    print("You order:",ch,"price = ",price,"THB")
+elif ch==2:
+    price=50
+    print("You order:",ch,"price = ",price,"THB")
+elif ch==3:
+    price=40
+    print("You order:",ch,"price = ",price,"THB")
+elif ch==4:
+    price=90
+    print("You order:",ch,"price = ",price,"THB")
+elif ch==5:
+    price=10
+    print("You order:",ch,"price =",price,"THB")
+else:
+    print("You not order:")
+
+
+# In[ ]:
+
+
+
+
